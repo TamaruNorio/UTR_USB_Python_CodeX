18cbe672aa88e304d5f6d7141c6590052feb2065
田丸 典億 <tamaru@ad.takaya.co.jp>
2026-06-16 09:47:11 +0900

    Change default baud rate to 115200

--- tests/test_serial_port_helpers.py
+++ tests/test_serial_port_helpers.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
+import sys
+import types
 
 import src.utr_serial_ports as serial_ports
+
+
+serial_module = types.ModuleType("serial")
+serial_module.Serial = object
+serial_module.SerialException = Exception
+serial_module.EIGHTBITS = 8
+serial_module.PARITY_NONE = "N"
+serial_module.STOPBITS_ONE = 1
+serial_tools_module = types.ModuleType("serial.tools")
+serial_list_ports_module = types.ModuleType("serial.tools.list_ports")
+serial_list_ports_module.comports = lambda: []
+serial_tools_module.list_ports = serial_list_ports_module
+serial_module.tools = serial_tools_module
+sys.modules.setdefault("serial", serial_module)
+sys.modules.setdefault("serial.tools", serial_tools_module)
+sys.modules.setdefault("serial.tools.list_ports", serial_list_ports_module)
+
+import src.utr_usb_sample as sample
 
 
 @dataclass
@@ -50,3 +70,23 @@ def test_find_port_by_user_input_returns_none_for_invalid_input():
 
     assert serial_ports.find_port_by_user_input("invalid", ports) is None
     assert serial_ports.find_port_by_user_input("9", ports) is None
+
+
+def test_parse_baud_rate_input_uses_115200_for_empty_input():
+    assert sample.parse_baud_rate_input("") == 115200
+
+
+def test_parse_baud_rate_input_accepts_115200_and_19200():
+    assert sample.parse_baud_rate_input("115200") == 115200
+    assert sample.parse_baud_rate_input("19200") == 19200
+
+
+def test_parse_baud_rate_input_ignores_surrounding_spaces():
+    assert sample.parse_baud_rate_input(" 115200 ") == 115200
+
+
+def test_parse_baud_rate_input_rejects_invalid_input():
+    import pytest
+
+    with pytest.raises(ValueError):
+        sample.parse_baud_rate_input("abc")
